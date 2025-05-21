1b6f6a3d3f004448b9cab4d9c71acd5d1bea86e3
Tamara Murazyan <murazyan.tamara5566.com>
2025-05-21 11:39:02 +0400

    user repository add

--- src/main/java/com/online/store/repository/UserRepository.java
+++ src/main/java/com/online/store/repository/UserRepository.java
@@ -3,5 +3,5 @@ package com.online.store.repository;
 import com.online.store.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends JpaRepository<User, Integer> {
 }
