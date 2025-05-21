fc56bf545a33ffd3267517967540f01ef686e4a2
Tamara Murazyan <murazyan.tamara5566.com>
2025-05-21 14:21:58 +0400

    user service update

--- src/main/java/com/online/store/service/UserService.java
+++ src/main/java/com/online/store/service/UserService.java
@@ -3,16 +3,18 @@ package com.online.store.service;
 import com.online.store.dto.UpdateUserRequest;
 import com.online.store.dto.UserDto;
 import com.online.store.entity.User;
+import com.online.store.mapper.UserMapper;
 import com.online.store.repository.UserRepository;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import org.springframework.stereotype.Service;
 
+@AllArgsConstructor
 @Service
 public class UserService {
+    private final UserMapper userMapper;
     private final UserRepository userRepository;
 
-    public UserService(UserRepository userRepository) {
-        this.userRepository = userRepository;
-    }
 
     public void update(UpdateUserRequest request, User user) {
         if (request.getUsername() != null) {
@@ -22,13 +24,6 @@ public class UserService {
             user.setEmail(request.getEmail());
         }
     }
-    private UserDto mapToDTO(User user) {
-        UserDto dto = new UserDto();
-        dto.setId(user.getId());
-        dto.setUsername(user.getUsername());
-        dto.setEmail(user.getEmail());
-        return dto;
-    }
 
     public UserDto updateUser(int id, UpdateUserRequest request) {
         User user = userRepository.findById(id)
@@ -37,6 +32,6 @@ public class UserService {
 
         User updatedUser = userRepository.save(user);
 
-        return mapToDTO(updatedUser);
+        return userMapper.toDto(updatedUser);
     }
 }
